9f0ae3293fdf774ee36040a3348dcc6fc71a15bf
Nikita Dmitriev <dmitrevnik@gmail.com>
2025-09-08 10:10:25 +0300

    Create 1317-ConvertIntegerToTheSumOfTwoNo-ZeroIntegers.js

---
+++ src/1317-ConvertIntegerToTheSumOfTwoNo-ZeroIntegers.js
@@ -0,0 +1,12 @@
+var getNoZeroIntegers = function(n) {
+    function check(x) {
+        return !x.toString().includes('0');
+    }
+
+    for (let i = 1; i < n; i++) {
+        let j = n - i;
+        if (check(i) && check(j)) {
+            return [i, j];
+        }
+    }
+};
